831a8a6d72e41ddd04090bfb350264e7e9ffd38a
sallaball <sallaball1206@gmail.com>
2022-06-07 17:07:47 -0500

    made sure new enemy is picked after last one died in battle

--- assets/js/game.js
+++ assets/js/game.js
@@ -23,8 +23,9 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) {
-    //Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //repeat and execute as long as the enemy-robot is alive
+    while(enemyHealth > 0) {
+
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth` and use that result to update the value in the `enemyHealth` variable
 
     var promptFight = window.prompt("Would you like to FIGTH or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -63,6 +64,7 @@ else {
 }
 }
 
+
 else if (promptFight === "skip" || promptFight === "SKIP") {
     // confirm player wants to skip
     var confirmSkip = window.confirm("Are you sure you'd like to quit?");
@@ -83,8 +85,12 @@ else if (promptFight === "skip" || promptFight === "SKIP") {
     window.alert("You need to choose a valid option. Try again!");
 }
 }
+}
 
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
+
