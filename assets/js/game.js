b9093ed8c8afcc37035ce1af2cf9c1b2ee8011cd
sallaball <sallaball1206@gmail.com>
2022-06-06 20:44:14 -0500

    little update

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,8 @@
-window.alert("This is an alert! JavaScript is running");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+
+window.prompt("What is your robot's name?");
