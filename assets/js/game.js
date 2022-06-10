e342ed27c8371a09f63f123614736d4489d682a3
sallaball <sallaball1206@gmail.com>
2022-06-10 15:18:39 -0500

    working on it

--- assets/js/game.js
+++ assets/js/game.js
@@ -151,21 +151,18 @@ var endGame = function() {
 
 };
 var shop = function() {
-  var shopOptionPrompt = window.prompt('Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.');
-
+  var shopOptionPrompt = window.prompt('Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "1" for REFILL, "2" for UPGRADE, or "3" to LEAVE to make a choice.');
+    shopOptionPrompt = parseInt(shopOptionPrompt);
         switch (shopOptionPrompt) {
-          case "refill":
-          case "REFILL":
+          case "1":
             playerInfo.refillHealth();
             break;
 
-            case "upgrade":
-            case "UPGRADE":
+            case "2":
              playerInfo.upgradeAttack();
               break;
 
-              case "leave":
-              case "LEAVE":
+              case "3":
                 window.alert("Leaving the store.");
                 break;
 
